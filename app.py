503ac38dad6d31f20a0d07f3f365fb14e3e8a1a0
DAKSie <redygrassy09@gmail.com>
2025-10-16 12:50:17 +0800

    yessir

--- app.py
+++ app.py
@@ -43,5 +43,5 @@ def get_affirmation():
     return jsonify(affirmations), 200
 
 if __name__ == "__main__":
-    # port = int(os.environ.get("PORT", 5000))
-    app.run(debug=False)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host = '0.0.0.0', port = port, debug=False)
